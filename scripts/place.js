36a2093a0e826895e5927cfe3e1607eed643f0d1
wddbyuidaho <mmirellaisabel@yahoo.es>
2024-06-29 20:42:19 -0500

    task progress week 3

---
+++ scripts/place.js
@@ -0,0 +1,10 @@
+document.getElementById("year").innerHTML = (new Date().getFullYear());
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+
+function calculateWindChill(temperature, windspeed){
+
+}
+let get_weather =() =>{
+    let url=""
+}
